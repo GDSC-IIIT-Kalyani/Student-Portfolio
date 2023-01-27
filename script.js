5837db74cbf11ca2b8284cb043bf7081f4ecccf1
ImanKalyanChakraborty <imankc1975@gmail.com>
2023-01-27 15:31:27 +0530

    made more changes
    desc

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline();
 
+tl.to("#element-1", {
+  opacity: 0,
+  duration: 1,
+})
+
+tl.to("#elementPurgatory", {
+  opacity: 0,
+  duration: 1,
+})
+
 tl.from("#element0", {
   opacity: 0,
   duration: 1,
 })
 
 tl.from("#element1", {
-  y: '-100vh',
+  y: '-30vh',
   opacity: 0,
   duration: 1,
 })
 
 tl.from("#element2", {
-  y: '100vh',
+  y: '30vh',
   opacity: 0,
   duration: 1,
 });
@@ -24,4 +34,29 @@ ScrollTrigger.create({
   trigger: ".page1",
   scrub: true,
   pin: true
+})
+
+const tl2 = gsap.timeline();
+
+ScrollTrigger.create({
+  animation: tl2,
+  trigger: ".page2",
+  start: "top top",
+  scrub: true,
+  pin: true
+})
+
+tl2.from("#element3", {
+  x: '-30px',
+  opacity: 0
+})
+
+tl2.from("#element4", {
+  x: '-30px',
+  opacity: 0
+})
+
+tl2.from("#element5", {
+  x: '30px',
+  opacity: 0
 })
